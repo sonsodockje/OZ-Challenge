46b75fdb5f9d69645b6650ef5b52c6d71e6e093b
sonsodockje <hanaroo0115@naver.com>
2023-12-29 14:02:25 +0900

    ch14

--- FE_JS/challenge/challenge_14_나머지가1.js
+++ FE_JS/challenge/challenge_14_나머지가1.js
@@ -1,27 +1,15 @@
-/*
- * ❗️❗️ 필독 ❗️❗️
- * - 함수 블록 내부에만 작성해주세요.
- * - 결과값을 return 하세요.
- * - 추가적인 test가 필요한 경우에는 기존 test 코드 밑에 추가해주세요.
- * - 코드 실행 시 모든 테스트가 true인 경우 통과입니다!
- */
-
 /*  문제 출제
  *  자연수 n이 인수로 주어질 때, n을 나눠 나머지가 1이 되는 가장 작은 자연수를 구하는 함수를 구현해보세요!
- *
- *
- *
- *
  */
 
 function question(n) {
-  // 여기에서 코드 작성해주세요!
-  let result;
-
-  return result;
+  let result = 2;
+  while (true) {
+    if (n % result === 1) return result;
+    else result++;
+  }
 }
 
-// 여기는 결과값 함수이므로 신경쓰지 않으셔도 됩니다!
 Test(question, [[10], [12]], [3, 11]);
 
 function Test(question, conditions, results) {
