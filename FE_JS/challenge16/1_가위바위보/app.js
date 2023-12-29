ed012cba893e01abcbcab0cf629195a68484b388
sonsodockje <hanaroo0115@naver.com>
2023-12-30 00:25:58 +0900

    가바보

--- FE_JS/challenge16/1_가위바위보/app.js
+++ FE_JS/challenge16/1_가위바위보/app.js
@@ -4,10 +4,11 @@ let img = [
   "https://image.auction.co.kr/itemimage/28/65/8e/28658ea5e6.jpg",
   "https://previews.123rf.com/images/ampak/ampak1112/ampak111200008/11724275-%EC%86%90%EC%9D%84-%EC%86%90%EB%B0%94%EB%8B%A5%EC%9D%B4-%EB%8F%84%EC%9B%80%EC%9D%84-%EC%A0%9C%EA%B3%B5.jpg",
 ];
-let userChoice = "";
-
+// 위에거 객체로 스정 하면 될듯
 let gameResultText = document.getElementById("gameResultText");
-let userBtnChoice = document.querySelectorAll(".userChoice");
+let userChoice = document.querySelectorAll(".userChoice");
+let winCount = 0;
+let count = 0;
 
 // 중복 줄이기 ----------------------------------------------------------
 // for (let i = 0; i < userBtnChoice.length; i++) {
@@ -17,14 +18,13 @@ let userBtnChoice = document.querySelectorAll(".userChoice");
 // }
 
 // forEach로 모든 버튼에 이벤트 리스너 등록 ----------------------------------
-userBtnChoice.forEach(function (button) {
+userChoice.forEach(function (button) {
   button.addEventListener("click", function (e) {
     choice(e.target.value, e.target.attributes["number"].value);
   });
 });
 
-function choice(userBtnChoice, userImgNumber) {
-  let userChoice = userBtnChoice;
+function choice(userChoice, userImgNumber) {
   let userChoiceImg = img[userImgNumber];
 
   let rn = Math.floor(Math.random() * 3);
@@ -44,18 +44,26 @@ function choice(userBtnChoice, userImgNumber) {
                                   </div>
                                 </div>                         
                                 <h1>${result}</h1>
+                                <h1>${count} 번 경기중 ${winCount} 번 이김.</h1>
                             `;
 }
 
 function winner(userChoice, computerChoice) {
-  if (userChoice === computerChoice) return "비김";
+  if (userChoice === computerChoice) {
+    count++;
+    return "비김";
+  }
   if (
     (userChoice === "가위" && computerChoice === "보") ||
     (userChoice === "바위" && computerChoice === "가위") ||
     (userChoice === "보" && computerChoice === "바위")
   ) {
+    count++;
+    winCount++;
+
     return "님 이김";
   } else {
+    count++;
     return "님 짐";
   }
 }
