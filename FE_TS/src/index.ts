079022f8203ab084c43f3d294815251546dbcf8d
sonsodockje <hanaroo0115@naver.com>
2024-01-16 18:28:25 +0900

    ts

---
+++ FE_TS/src/index.ts
@@ -0,0 +1,62 @@
+// const toggle = (option: "on" | "off") => {
+//   console.log(option);
+// };
+
+// toggle("on");
+
+// -----------------------------------------------------
+
+// type SuccessCode = 200 | 201 | 202;
+// type ErrorCode = 500 | 501;
+
+// let rep: SuccessCode | ErrorCode;
+// rep = 500;
+
+// -----------------------------------------------------
+
+// interface Car {
+//   user: string;
+//   drive(input: string): void;
+// }
+
+// let car: Car = {
+//   user: "min",
+//   drive(input) {
+//     console.log(`${this.user} 님의 ${input} 메시지.`);
+//   },
+// };
+
+// car.drive("메롱");
+
+// -----------------------------------------------------
+
+// interface Product {
+//   name: string;
+//   price?: number;
+// }
+
+// let product: Product = {
+//   name: "banana",
+//   price: 100,
+// };
+
+// let product2: Product = {
+//   name: "apple",
+// };
+
+// console.log(product);
+// console.log(product2);
+
+// -----------------------------------------------------
+
+// interface Vehicle {
+//   readonly serialNumber: string;
+//   type: string;
+// }
+
+// let car: Vehicle = {
+//   serialNumber: "d2543",
+//   type: "45423",
+// };
+
+// -----------------------------------------------------
