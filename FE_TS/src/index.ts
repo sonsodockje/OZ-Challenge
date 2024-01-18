222effbdb4b0de8adfae4a7375ffe7386677c36b
sonsodockje <hanaroo0115@naver.com>
2024-01-18 17:12:26 +0900

    ts-큐

--- FE_TS/src/index.ts
+++ FE_TS/src/index.ts
@@ -1,8 +1,17 @@
-// push 메서드로 새로운 요소를 큐의 뒤쪽에 추가하고, shift 메서드로 가장 앞쪽의 요소, 즉 가장 오래된 요소를 제거합니다.
+class Queue<T> {
+  #items: T[] = [];
 
-// 여기에 Queue<T> 클래스를 구현하세요.
+  enqueue(item: T): void {
+    this.#items.push(item);
+  }
+  dequeue(): T | undefined {
+    return this.#items.shift();
+  }
+  size(): number {
+    return this.#items.length;
+  }
+}
 
-// 사용 예시
 const queue = new Queue<string>();
 queue.enqueue("Apple");
 queue.enqueue("Banana");
